ffa28668e697df57922008c00eaabc64971a77c5
melnykyura98 <melnykyura1998@gmail.com>
2024-08-12 11:03:25 +0300

    fix: config

--- src/routes/bids.ts
+++ src/routes/bids.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import bidsController from "../controllers/bids";
-import teamsMiddleware from "../middlewares/teams";
-import permissionsMiddleware from "../middlewares/permissions";
 
 const router = Router();
 
@@ -9,9 +7,6 @@ router
     .get("/",
         bidsController.getBids
     )
-    // .param("bidId",
-    //     Middleware.findTeam
-    // )
     .get("/:bidId",
         bidsController.getBidById
     )
